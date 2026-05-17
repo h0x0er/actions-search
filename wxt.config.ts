e380e264b5e1b508182b150cfd9ee8ad0ed1b48d
Jatin <84621253+h0x0er@users.noreply.github.com>
2026-05-17 23:40:07 +0530

    fixed data collection perms and bump to 004

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,16 +6,18 @@ export default defineConfig({
     name: "Actions Workflow Search",
     description: "Search workflows in the GitHub Actions sidebar.",
     homepage_url: "https://github.com/h0x0er/actions-search",
-    version: "0.0.3",
+    version: "0.0.4",
     host_permissions: ["https://github.com/*"],
     browser_specific_settings: {
       gecko: {
         id: "{B7F12730-240E-4F6C-A874-74017FCC6B97}",
         strict_min_version: "109.0",
+        ...(browser === "firefox" && {
+          data_collection_permissions: {
+            required: ["none"],
+          },
+        }),
       },
     },
-    ...(browser === "firefox" && {
-      data_collection_permissions: { required: [], optional: [] },
-    }),
   }),
 });
